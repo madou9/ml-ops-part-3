ffe8a4e09801684fdbe8774d75a59a88ddae7271
level3 <level3@admins-Mac-mini.local>
2025-05-27 16:36:51 +0200

    initialise my ml pipeline for the project

--- ml_pipeline/__init__.py
+++ ml_pipeline/__init__.py
@@ -1,7 +1,12 @@
 # ml_pipeline/__init__.py
 from dagster import Definitions
 from .assets import data, features, model, evaluation
+from .io_managers.lakefs_io_manager import lakefs_io_manager
+from .config.pipeline_config import get_lakefs_config
 
 defs = Definitions(
     assets=[*data.assets],
+     resources={
+        "lakefs_io_manager": lakefs_io_manager.configured(get_lakefs_config())
+    }
 )
